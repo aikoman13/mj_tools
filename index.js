91a908201550f9b27be854ec6c9e38c34cf8ebee
Aikoman <126990322+aikoman13@users.noreply.github.com>
2023-03-09 13:20:39 -0400

    mody the accourion effect

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
+    // close any open sections
+    var activePanels = document.querySelectorAll(".accordion.active + .panel");
+    activePanels.forEach(function(panel) {
+      panel.style.display = "none";
+      panel.previousElementSibling.classList.remove("active");
+    });
+    
+    // toggle the clicked section
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
